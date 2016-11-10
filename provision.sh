8b0f807be77a5441c7d9a8b122be3e5831a81e8f
Jake Bladt <jake@jakebladt.com>
2016-11-10 12:03:28 -0500

    -elevated permissions

--- provision.sh
+++ provision.sh
@@ -3,23 +3,23 @@ apt-get update
 
 # scala install
 wget www.scala-lang.org/files/archive/scala-2.11.7.deb
-sudo dpkg -i scala-2.11.7.deb
+dpkg -i scala-2.11.7.deb
 
  
 # sbt installation
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update
-sudo apt-get install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+apt-get update
+apt-get install sbt
 
 # java install
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
 
 # git install
-sudo apt-get install git
+apt-get install git
 
 # install python tools for web front ends
 apt-get install python3-setuptools
